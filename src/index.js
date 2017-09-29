bbe36ce7a5c26905bd9ba4ceafbc0832a3f79ff1
lindenii <35da9s1ha>
2017-09-29 12:24:45 +0300

    task5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0)
+        return false;
+    str = str.split('');
+    var mas = [];
+    var i ;
+    var j ;
+    for ( i = 0; i < str.length; i++) {
+
+        for ( j = 0; j < bracketsConfig.length; j++) {
+
+            if ((mas[mas.length - 1] === bracketsConfig[j][0]) && (str[i] === bracketsConfig[j][1]))
+               mas.pop();
+
+            else
+                if (str[i] === bracketsConfig[j][0])
+                    mas.push(str[i]);
+        }
+    }
+    return mas.length === 0;
+};
